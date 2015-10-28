485021256e7bebe1569fb88e2a56b8672505ebc7
Jithu R Jacob <jithurjacob@gmail.com>
2015-10-28 22:14:57 +0530

    tone api ok

--- server.py
+++ server.py
@@ -10,39 +10,24 @@ def index():
 def tone():
 	
 	try:
-		#Check if app is in BlueMix Environment
-		if 'VCAP_SERVICES'  in os.environ:
-			vcap_services = []#json.loads(os.environ['VCAP_SERVICES'])
-			svcName="tone_analyzer"
-			if svcName  in vcap_services:
-				#svc = vcap_services[svcName][0]['credentials']
-				url = "https://gateway.watsonplatform.net/tone-analyzer-experimental/api/v1/tone"#"svc['url']+"
-				user = "10aef9f7-07da-4b65-aac8-ab977b85f244"#svc['username']
-				password = "RbCp8gfylAWo"#svc['password']
-				data={'text': (request.form['text'])}
-				r = requests.post(url,auth=(user,password),headers = {'content-type': 'application/json'},data=json.dumps(data))
-				if r.status_code!=200:
-					try:
-						error = json.loads(r.text)
-					except Exception, e:
-						return "API error, http status code %d" % r.status_code
-						#raise Exception("API error, http status code %d" % r.status_code)
-						#raise Exception("API error %s: %s" % (error['error_code'], error['user_message']))
-				data=json.loads(r.text)
-				return json.dumps(data)
-			else:
-				srvcs=[]
-				for srv in vcap_services:
-					srvcs.append(srv)
-				message = {'message': "Service is not present"+" ".join(srvcs)}
-				return make_response(json.dumps(message),200)
-				
-		else:
-			return ("VCAP is none")
+		url = "https://gateway.watsonplatform.net/tone-analyzer-experimental/api/v1/tone"#"svc['url']+"
+		user = "10aef9f7-07da-4b65-aac8-ab977b85f244"#svc['username']
+		password = "RbCp8gfylAWo"#svc['password']
+		data={'text': (request.form['text'])}
+		r = requests.post(url,auth=(user,password),headers = {'content-type': 'application/json'},data=json.dumps(data))
+		if r.status_code!=200:
+			try:
+				error = json.loads(r.text)
+			except Exception, e:
+				return "API error, http status code %d" % r.status_code
+		data=json.loads(r.text)
+		return json.dumps(data)
+		
 	except Exception, e:
-		return ('Error'+str(e))
+		message = {'message': "Excption "+str(e)}
+		return make_response(json.dumps(message),200)
 
-	return 'Ok'
+	
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(port))
